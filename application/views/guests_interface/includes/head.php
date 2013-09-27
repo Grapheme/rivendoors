f8aceb27810263ef764153fd10e8aa5cf2e8d118
Artem <reaper90fang@gmail.com>
2013-09-27 19:20:14 +0400

    fancybox, new slider, mass main.js changes, need fix+

--- application/views/guests_interface/includes/head.php
+++ application/views/guests_interface/includes/head.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <title><?=$this->page_variables->getPageTitle();?></title>
 <meta name="description" content="<?=$this->page_variables->getPageDescription();?>" />
-<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 <link rel="icon" href="<?=base_url('favicon.ico');?>" />
 <link rel="stylesheet" href="<?=base_url('css/normalize.css');?>" />
 <link rel="stylesheet" href="<?=base_url('css/fonts.css');?>" />
@@ -11,8 +11,4 @@
 <link type="text/css" href="<?=base_url('css/jquery.jscrollpane.css');?>" rel="stylesheet" media="all" />
 
 <script src="<?=base_url('js/vendor/modernizr-2.6.2.min.js');?>"></script>
-<script type="text/javascript" src="//vk.com/js/api/openapi.js?97"></script>
 
-<script type="text/javascript">
-  VK.init({apiId: 3771737, onlyWidgets: true});
-</script>
--- js/cabinet/jcarousel-config.js
+++ js/cabinet/jcarousel-config.js
@@ -1,9 +1,66 @@
-$(function(){
-	$('.jcarousel').jcarousel();
-	$('.jcarousel-prev').jcarouselControl({target : '-=1'});
-	$('.jcarousel-next').jcarouselControl({target : '+=1'});
-	$('.jcarousel-prev').on('inactive.jcarouselcontrol', function(){$(this).css({ opacity: '0.2' });});
-	$('.jcarousel-prev').on('active.jcarouselcontrol', function(){$(this).css({ opacity: '1' });});
-	$('.jcarousel-next').on('inactive.jcarouselcontrol', function(){$(this).css({ opacity: '0.2' });});
-	$('.jcarousel-next').on('active.jcarouselcontrol', function(){$(this).css({ opacity: '1' });});
+$(function(){	
+	/* Set items width before carousel initialization*/
+	
+	function setLiWidth() {
+		$('#foo li').each( function(){
+			$(this).width( $(this).height() / 1.61 ).css({ overflow: 'hidden' });
+		});
+	}
+	
+	function carouselInit() {
+		setLiWidth();
+		$("#foo").after('<ul id="fooX" class="clearfix" />').next().html($("#foo").html());
+		$("#foo li:odd").remove();
+		$("#fooX li:even").remove();
+		$("#foo").carouFredSel({
+			width: '100%',
+			align: 'left',
+			auto: false,
+			circular: false,
+			infinite: false,
+	    	prev : {
+				button : $('.jcarousel-prev'),
+				key: 'left'
+			},
+			next : {
+				button : $('.jcarousel-next'),
+				key: 'right'
+				
+			},
+			items : { 
+				visible : {
+					min: 1,
+					max: 10
+				} 
+			},
+			synchronise : "#fooX"
+		});
+		
+		$("#fooX").carouFredSel({
+			width: '100%',
+			align: 'left',
+			auto: false,
+			circular: false,
+			infinite: false
+		});
+		
+		$('.fancybox').fancybox({
+			maxHeight: 500,
+			padding: 0,
+			'autoScale' : false, 
+			'type' : 'image',
+			helpers		: {
+				title	: { type : 'inside', position: 'top' }
+			},
+			items : {
+		        height : "100%",
+		        visible : {
+		            min : 1,
+		            max : 10
+		        }
+	    	}
+		});
+	}
+	/*setLiWidth();*/
+	carouselInit();
 });
