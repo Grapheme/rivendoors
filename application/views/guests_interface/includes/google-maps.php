9b2d6d88cabd474e33f2b643e3367a3fede9fa6b
Andrey S <andrey@grapheme.ru>
2014-01-20 18:05:33 +0400

    remove dot on the map temp

--- application/views/guests_interface/includes/google-maps.php
+++ application/views/guests_interface/includes/google-maps.php
@@ -9,12 +9,14 @@
 		}
 		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 		var image = 'img/interface/map_button.png';
+		/*
 		var myLatLng = new google.maps.LatLng(47.210275, 39.656423);
 		var beachMarker = new google.maps.Marker({
 			position : myLatLng,
 			map : map,
 			icon : image
 		});
+		*/
 	}
 	initialize();
 </script>
